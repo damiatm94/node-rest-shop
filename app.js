bb447fba59af914e6debae882606f1eceb897ce0
damiatm94 <damkamg94@gmail.com>
2018-10-16 23:59:05 +0200

    added simple cors handling

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express(); // thanks that we can use all kind of utilities and so on
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 const morgan = require('morgan'); // this is a package to logging something
+const bodyParser = require('body-parser');
 
 // incoming request has to go to app.use
 // app.use((req, res, next) => {
@@ -13,6 +14,17 @@ const morgan = require('morgan'); // this is a package to logging something
 // }); 
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Contetn-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 // routes which should handle requests
 app.use('/products', productRoutes);
