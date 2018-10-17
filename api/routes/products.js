5339fea344db22c9e06f2b61216d6b3bcdaa433d
damiatm94 <damkamg94@gmail.com>
2018-10-17 21:09:22 +0200

    Added mongoose

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,35 +1,68 @@
 const express = require('express');
 const router = express.Router(); // allows us to handle different routes etc
+const Product = require('../models/product');
+
+const mongoose = require('mongoose');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /products'
-    })
+    Product.find()
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            // docs.length >= 0 ? res.status(200).json(docs) : res.status(404).json({message: 'No entries found'});
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.statas(500).json({ error: err });
+        });
 });
 
 router.post('/', (req, res, next) => {
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
-    res.status(200).json({
-        message: 'Handling POST requests to /products',
-        createProduct: product
     })
+
+    // save is a method provided by mongoose. We can use this method on mongoose models
+    // save method will store this object in the database
+    product.save()
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                message: 'Handling POST requests to /products',
+                createProduct: product
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
 });
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+            console.log('From database:', doc);
+            doc ? res.status(200).json(doc) : res.status(404).json({ message: 'No valid data for provided ID' });
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: err });
+        });
 
-    const specialMessage = {
-        message: 'You discovered the special ID',
-        id: id
-    };
-    const normalMessage = {
-        message: 'You passed an ID'
-    };
+    // const specialMessage = {
+    //     message: 'You discovered the special ID',
+    //     id: id
+    // };
+    // const normalMessage = {
+    //     message: 'You passed an ID'
+    // };
 
-    id === 'special' ? res.status(200).json(specialMessage) : res.status(200).json(normalMessage);
+    // id === 'special' ? res.status(200).json(specialMessage) : res.status(200).json(normalMessage);
 });
 
 router.patch('/:productId', (req, res, next) => {
@@ -39,9 +72,16 @@ router.patch('/:productId', (req, res, next) => {
 });
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted product!'
-    });
+    const id = req.params.productId;
+    Product.remove({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: err });
+        });
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express(); // thanks that we can use all kind of utilities and so one
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
 const morgan = require('morgan'); // this is a package to logging something
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect(
+    'mongodb://node-shop:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop-shard-00-00-crqrh.mongodb.net:27017,'
+    + 'node-rest-shop-shard-00-01-crqrh.mongodb.net:27017,node-rest-shop-shard-00-02-crqrh.'
+    + 'mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true',
+    {
+        useMongoClient: true
+    });
 
 // incoming request has to go to app.use
 // app.use((req, res, next) => {
